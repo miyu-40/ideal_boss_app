7cfd986c2ff0d5b361e3b5b64c84d2c8eef7735d
miyu-40 <o.mi.089514@gmail.com>
2023-06-22 18:05:16 +0900

    Add: パスワードリセット

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/create'
+  get 'password_resets/edit'
+  get 'password_resets/update'
   root "top#index"
   resources :users, only: [:new, :create]
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
+  resources :password_resets, only: %i[new create edit update]
 end
